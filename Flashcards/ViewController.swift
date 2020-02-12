337bc1bf7550272b1aa7c4fc30556d6260840683
Zoe Offermann <zoffermann15>
2020-02-12 04:49:47 -0500

    Finished up the optional work.

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -14,16 +14,49 @@ class ViewController: UIViewController {
     @IBOutlet weak var frontLabel: UILabel!
     @IBOutlet weak var card: UIView!
     
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    func buttonSetUp(button: UIButton){
+        button.layer.borderWidth = 3.0
+        button.layer.borderColor = #colorLiteral(red: 0.5568627715, green: 0.3529411852, blue: 0.9686274529, alpha: 1)
+        button.layer.cornerRadius = 15.0
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        card.layer.cornerRadius = 20.0
-        card.clipsToBounds = true
-        // Do any additional setup after loading the view.
+        card.layer.shadowOpacity = 0.5
+        card.layer.shadowRadius = 20.0
+        frontLabel.clipsToBounds = true
+        backLabel.clipsToBounds = true
+        frontLabel.layer.cornerRadius = 20.0
+        backLabel.layer.cornerRadius = 20.0
+        
+        buttonSetUp(button: button1)
+        buttonSetUp(button: button2)
+        buttonSetUp(button: button3)
+
+
+
+
     }
 
+    @IBAction func button1Press(_ sender: Any) {
+        frontLabel.isHidden = true
+    }
+    
+    @IBAction func button2Press(_ sender: Any) {
+        button2.isHidden = true
+    }
+    
+    
+    @IBAction func button3Press(_ sender: Any) {
+        button3.isHidden = true
+    }
+    
     @IBAction func didTapCard(_ sender: Any) {
-        print("I have clicked")
-        print(frontLabel.isHidden)
+
         frontLabel.isHidden = !frontLabel.isHidden
     }
     
