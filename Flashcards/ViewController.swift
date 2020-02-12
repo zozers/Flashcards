e15f9d486edcda136885d2e82f0b6bda74c6be9a
Zoe Offermann <zoffermann15>
2020-02-12 03:30:42 -0500

    Working on optional part of the lab

--- Flashcards/ViewController.swift
+++ Flashcards/ViewController.swift
@@ -9,18 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var frontLabel: UILabel!
+        
     @IBOutlet weak var backLabel: UILabel!
+    @IBOutlet weak var frontLabel: UILabel!
+    @IBOutlet weak var card: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        card.layer.cornerRadius = 20.0
+        card.clipsToBounds = true
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func didTapFlashcard(_ sender: Any) {
+    @IBAction func didTapCard(_ sender: Any) {
+        print("I have clicked")
+        print(frontLabel.isHidden)
         frontLabel.isHidden = !frontLabel.isHidden
     }
     
-
 }
 
